ef1c9253706edcac1bcbee5700aa888d3190de15
Jordan <jordanace83@gmail.com>
2021-02-09 17:58:53 -0600

    Complete connecting combine reducer to store

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,6 +1,15 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
+
+  if (action.type === 'CHANGE_VIDEO') {
+    return action.video;
+
+  } else {
+    return state;
+  }
+
+
   //TODO: define a reducer for the currentVideo field of our state.
 };
 
