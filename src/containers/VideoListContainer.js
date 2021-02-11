06e993d7cb1eba0c0a0279b9fe42a3d856be6589
Jordan <jordanace83@gmail.com>
2021-02-10 18:26:54 -0600

    Complete replacing components with containers

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -14,7 +14,7 @@ var mapDispactchToProps = (dispatch) => {
   };
 };
 
-//var VideoListContainer = () => {};
+
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
